1074a2a46003ba79ecebaf030e1bce43e40fe3a5
Abi Devops <aik0002@uah.edu>
2021-09-03 12:18:46 -0500

    new commit 9/3/21

--- mavenproject1/src/main/java/newmain/NewMainaa.java
+++ mavenproject1/src/main/java/newmain/NewMainaa.java
@@ -16,6 +16,7 @@ public class NewMainaa {
      */
     public static void main(String[] args) {
         System.out.println("Hello World I am trying"); //no
+        System.out.println("Hello World I am trying"); //no
     }
     
 }
